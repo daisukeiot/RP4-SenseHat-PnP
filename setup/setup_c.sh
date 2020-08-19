eb60f3a7e709d40eebd049a9e17eecba45812382
Daisuke Nakahara <daisuken@msn.com>
2020-08-19 07:53:43 +0100

    Add scripts

--- setup/setup_c.sh
+++ setup/setup_c.sh
@@ -3,5 +3,5 @@ cd ~/RP4-SenseHat-PnP/C && \
 git clone --depth 1 --recurse-submodules https://github.com/Azure/azure-iot-sdk-c.git && \
 mkdir cmake && \
 cd cmake && \
-cmake .. -Dskip_samples=ON -Dhsm_type_symm_key=ON -Dbuild_service_client=OFF -Dbuild_provisioning_service_client=OFF -Duse_http=OFF -Duse_amqp=OFF
-```
+cmake .. -Dskip_samples=ON -Dhsm_type_symm_key=ON -Dbuild_service_client=OFF -Dbuild_provisioning_service_client=OFF -Duse_http=OFF -Duse_amqp=OFF && \
+cmake --build .
